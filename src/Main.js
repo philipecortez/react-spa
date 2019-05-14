1d9c16e6f0cc30f62c32b59bf12a89c84498f963
Philipe Cortez <philipe.sousa@locaweb.com.br>
2019-05-14 00:10:18 -0300

    finishing the little SPA

--- src/Main.js
+++ src/Main.js
@@ -13,12 +13,14 @@ const Main = () => (
     <>
       <h1>Simple SPA</h1>
       <ul className='header'>
-        <li><NavLink to='/'>Home</NavLink></li>
+        <li><NavLink exact to='/'>Home</NavLink></li>
         <li><NavLink to='/stuff'>Stuff</NavLink></li>
         <li><NavLink to='/contact'>contact</NavLink></li>
       </ul>
       <div className='content'>
-
+        <Route exact path='/' component={Home} />
+        <Route path='/stuff' component={Stuff} />
+        <Route path='/contact' component={Contact} />
       </div>
     </>
   </HashRouter>
